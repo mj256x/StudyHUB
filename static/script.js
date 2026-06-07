2ae7cfc82de64182df9b84cdebfb23957e840a6a
mj256x <mohammed256jamal@gmail.com>
2026-06-08 01:00:38 +0300

    add file download functionality

--- static/script.js
+++ static/script.js
@@ -5,3 +5,22 @@ setTimeout(function () {
         flashMessage.style.display = 'none';
     }
 }, 3000);
+
+async function downloadFile(btn, fileUrl, fileName) {
+    try {
+        const response = await fetch(fileUrl);
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        window.URL.revokeObjectURL(url);
+    }
+    catch (error) {
+        console.error('Error downloading file:', error);
+        alert('Failed to download file. Please try again later.');
+    }
+}
