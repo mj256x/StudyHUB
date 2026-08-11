3a808951edf302965ad556f2ade0aebafbae8e9a
mj256x <mohammed256jamal@gmail.com>
2026-08-11 20:38:58 +0300

    Add search functionality and enhance UI: implement live search for subjects and tasks; update styles for search input and buttons

--- static/script.js
+++ static/script.js
@@ -399,3 +399,29 @@ async function uploadProfilePicture(input) {
         }
     }
 }
+
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('input', function (event) {
+    const term = searchInput.value.toLowerCase().trim();
+    const cards = document.querySelectorAll('[data-searchable-item]');
+
+    cards.forEach(card => {
+        const cardTitle = card.querySelector('.card-title');
+        const cardText = card.textContent.toLowerCase();
+        console.log(card.style.display);
+        if (cardText.includes(term)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
+
+function toggleSearchInput() {
+    const searchInput = document.getElementById('search-input');
+    const isVisible = searchInput.classList.toggle('is-visible');
+    if (isVisible) {
+        searchInput.classList.add('searchable');
+        searchInput.focus();
+    }
+}
